cba6c311347320db8e789e0ce0412ae0d8e6ed65
Pa-fs <whgpals641@naver.com>
2024-08-08 11:28:41 +0900

    1216546413212365786453152

--- everycoffee/js/index.js
+++ everycoffee/js/index.js
@@ -10,6 +10,21 @@ $(document).ready(function () {
         slidesToShow: 1,       // 한 번에 보여줄 슬라이드 수
         slidesToScroll: 1      
     })
+
+    $('.').slick({
+        autoplay: true,        // 자동 재생 활성화
+        autoplaySpeed: 2000,   // 슬라이드 전환 시간 (밀리초 단위)
+        arrows: false,         // 다음 및 이전 버튼 숨기기
+        dots: false,           // 네비게이션 도트 숨기기
+        draggable: true,       // 드래그 가능
+        infinite: true,        // 무한 반복 모드 활성화
+        speed: 500,            // 전환 속도 (밀리초 단위)
+        slidesToShow: 1,       // 한 번에 보여줄 슬라이드 수
+        slidesToScroll: 1      
+    })
+
+
+
 });
 
 const productSlide = document.querySelector('.drag_slide');
@@ -54,3 +69,4 @@ productSlide.addEventListener('touchmove', (e) => {
     const walk = (x - startX) * 1; // Adjust the scroll speed
     productSlide.scrollLeft = scrollLeft - walk;
 });
+
